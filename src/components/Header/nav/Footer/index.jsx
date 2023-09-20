d50728889cceb47e2fce4e07387de6b72f3a0fa6
Rohan <rohanrk39357@gmail.com>
2023-09-20 09:50:30 +0530

    web

---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,13 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a>Behance</a>
+        <a>LinkedIn</a>
+        <a>Twitter</a>
+        <a>Github</a>
+
+    </div>
+  )
+}
