525418969b7ca09c3f1a9051fee78b2b757d2bf5
rohannrk <rohanrk39357@gmail.com>
2023-10-13 04:30:05 +0530

    Hero

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -6,47 +6,33 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import { slideUp } from './animation';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 export default function Home() {
 
-  const firstText = useRef(null);
-  const secondText = useRef(null);
-  const slider = useRef(null);
-  let xPercent = 0;
-  let direction = -1;
 
-  useLayoutEffect( () => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(slider.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        scrub: 0.25,
-        start: 0,
-        end: window.innerHeight,
-        onUpdate: e => direction = e.direction * -1
-      },
-      x: "-500px",
-    })
-    requestAnimationFrame(animate);
-  }, [])
-
-  const animate = () => {
-    if(xPercent < -100){
-      xPercent = 0;
-    }
-    else if(xPercent > 0){
-      xPercent = -100;
-    }
-    gsap.set(firstText.current, {xPercent: xPercent})
-    gsap.set(secondText.current, {xPercent: xPercent})
-    requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
-  }
 
   return (
     <motion.main variants={slideUp} initial="initial" animate="enter" className={styles.landing}>
       <div className={styles.hero}>
+        <div className={styles.container}>
 
+          <div className={styles.textcontainer}>
+            <div className={styles.herotext}>
+              <div className={styles.secondarytext}>
+                Hi I am Rohan,
+              </div>
+              <div className={styles.primarytext}>
+                Digital Designer and Developer.
+              </div>
+            </div>
+              <p>
+              Rohan is a digital designer based in Mumbai, India. I love to work on different aspect of design process like visual identity, design system bit of motion and so on. Have a project in mind you’re excited about? <span>Let’s chat.</span>
+              </p>
+          </div>
+
+          
+        </div>
       </div>
     </motion.main>
   )
