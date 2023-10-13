4fb7d99dee4e9ae2c3bc43d5ca505b4965cfb296
rohannrk <rohanrk39357@gmail.com>
2023-10-13 13:34:40 +0530

    Hero and nav

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -6,7 +6,7 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import { slideUp } from './animation';
 import { motion } from 'framer-motion';
-import Link from 'next/link';
+import Rounded from '../../common/RoundedButton';
 
 export default function Home() {
 
@@ -23,15 +23,18 @@ export default function Home() {
                 Hi I am Rohan,
               </div>
               <div className={styles.primarytext}>
-                Digital Designer and Developer.
+                Digital Designer & Developer.
               </div>
             </div>
+            <div className={styles.description}>
               <p>
-              Rohan is a digital designer based in Mumbai, India. I love to work on different aspect of design process like visual identity, design system bit of motion and so on. Have a project in mind you’re excited about? <span>Let’s chat.</span>
+                Rohan is a digital designer based in Mumbai, India. I love to work on different aspect of design process like visual identity, design system bit of motion and so on. Have a project in mind you’re excited about? <span>Let’s chat.</span>
               </p>
+            </div>
           </div>
-
-          
+          <Rounded className={styles.button}>
+            <p>View projects</p>
+          </Rounded>
         </div>
       </div>
     </motion.main>
